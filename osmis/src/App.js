3a52245002d71652c54b843926c5ae8300db8ea9
Ital956 <njagijim14@gmail.com>
2023-09-07 12:43:07 +0300

    Initial commit

---
+++ osmis/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import Register from "./components/pages/Register";
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
